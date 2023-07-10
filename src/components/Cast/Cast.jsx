e14770b6bdceb0da6a8fbcafaabb26e2481c823a
michalmazurdev <m.mazur40@gmail.com>
2023-07-10 16:47:41 +0200

    cast.jsx updated

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,15 +1,33 @@
-import { useEffect } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+
+const fetchCast = async movieid => {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/movie/${movieid}/credits?api_key=5e58d3162f5aafaf855cf7d900bbc361`
+  );
+  return response.data.cast;
+};
 
 export const Cast = () => {
   const { movieid } = useParams();
+  const [cast, setCast] = useState(null);
 
   useEffect(() => {
-    console.log(movieid);
+    const fetchData = async () => {
+      const fetchedMovie = await fetchCast(movieid);
+      // console.log(fetchCast(movieid));
+      setCast(fetchedMovie);
+    };
+    fetchData();
   }, [movieid]);
   return (
     <div>
       <span>'cast: '{movieid}</span>
+      <ul>
+        {cast &&
+          cast.map(person => <li key={person.id}>{person.original_name}</li>)}
+      </ul>
     </div>
   );
 };
